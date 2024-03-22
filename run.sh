bdb696dbe186fb8fa93884dfe94e0a781ad6e651
sanama <162118329+isanama@users.noreply.github.com>
2024-03-22 17:44:07 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-14501a3e-1c15-4dd4-8571-457ca8f5873b/)
 
 
 echo "$curl_output" >> Update.tsx
